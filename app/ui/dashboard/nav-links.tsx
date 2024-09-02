6cf3ee5d69a213b9ed46e6a6f93a8abf2b7b076b
Priyanshu Kumar Mandal <mandalpriyanshu22@gmail.com>
2024-09-02 13:13:25 +0545

    ch7-fetching data

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -25,14 +25,14 @@ export default function NavLinks() {
       icon: HomeIcon,
     },
     {
-      name: 'About',
-      href: '/about',
-      icon: UserGroupIcon,
+      name: 'Invoices',
+      href: '/invoices',
+      icon: DocumentDuplicateIcon,
     },
     {
-      name: 'Docs',
-      href: '/docs',
-      icon: DocumentDuplicateIcon,
+      name: 'Customers',
+      href: '/Customers',
+      icon: UserGroupIcon,
     },
   ];
 
@@ -41,18 +41,18 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon as React.ElementType; // Cast to React.ElementType
         return (
-          <Link key={link.name} href={link.href}>
-            <a
+          <Link key={link.name} href={link.href}
+            
               className={clsx(
                 'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
                 {
                   'bg-sky-100 text-blue-600': pathname === link.href,
                 }
-              )}
-            >
+              )} >
+            
               <LinkIcon className="w-6" />  {/* This should work now */}
               <p className="hidden md:block">{link.name}</p>
-            </a>
+           
           </Link>
         );
       })}
